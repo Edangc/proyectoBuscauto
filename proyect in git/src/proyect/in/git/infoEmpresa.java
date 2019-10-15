6bbb78feda1f1c56f5b42467a65f2d86e86137c7
Lalo <Lalo@Eduardo>
2019-10-15 00:17:28 -0300

    cambio de planes

---
+++ proyect in git/src/proyect/in/git/infoEmpresa.java
@@ -0,0 +1,17 @@
+
+package proyect.in.git;
+
+/**
+ *
+ * @author Lalo
+ */
+public class infoEmpresa {
+    private final ListaAutos listaAutos;
+    private final ListaDeUsuarios listaUsuarios;
+
+    public infoEmpresa(ListaAutos listaAutos, ListaDeUsuarios listaUsuarios) {
+        this.listaAutos = listaAutos;
+        this.listaUsuarios = listaUsuarios;
+    }
+       
+}
